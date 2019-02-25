6167e41fc6af1d74d4d1a81ae72fce87e52b79b8
unknown <ilya-79@yandex.ru>
2019-02-25 23:49:41 +0300

    5

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="thanks">
-    <h1>This is an thanks page</h1>
+    <md-card id="card-thanks">
+        <md-card-media>
+            <img src="/assets/thanks-pic.jpg" alt="Vue-Logo" class="logo-redirect">
+        </md-card-media>
+        <md-card-content>
+          <p class="thanks-content">
+              Your data has been successfully entered into the datebase!
+          </p>
+        </md-card-content>
+        <md-card-actions>
+            <router-link to="/">
+              <md-button class="md-raised md-primary">Home</md-button>
+            </router-link>
+        </md-card-actions>
+    </md-card>
   </div>
 </template>
+
+<style>
+  .thanks {
+    width: 35%;
+    min-width: 200px;
+    opacity: 0;
+    position: absolute;
+    left: 50%;
+    top: -50%;
+    transform: translate(-50%);
+    animation-name: thanks;
+    animation-duration: .5s;
+    animation-timing-function: linear;
+    animation-fill-mode: forwards;
+  }
+
+  @keyframes thanks {
+    from {
+      opacity: 0;
+      top: -50%;
+    }
+    to {
+      opacity: 1;
+      top: 16%;
+    }
+  }
+
+  .thanks-content {
+    font-size: 16px;
+    text-align: center;
+  }
+
+  #card-thenks {
+    background: rgba(255, 255, 255, 0.6);
+  }
+  
+</style>
